eac612256b9b09c29f41c00cde9e94a2b3787881
0Hoxy <tinyfinger@naver.com>
2024-06-27 00:22:22 +0900

    feature: Delete User

---
+++ backend/src/main/java/com/mysite/backend/controller/UserController.java
@@ -0,0 +1,56 @@
+package com.mysite.backend.controller;
+
+import com.mysite.backend.entity.User;
+import com.mysite.backend.exception.UserNotFoundException;
+import com.mysite.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.file.Path;
+import java.util.List;
+
+@RestController
+//설정 후엔 꼭 업데이트 시켜주기 (저장만 해서는 안됌)
+@CrossOrigin("http://localhost:3000")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser) {
+        return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+
+    @PutMapping("/user/{id}")
+    User updateUser(@PathVariable Long id, @RequestBody User newUser) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                }).orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable Long id) {
+        if (!userRepository.existsById(id)) {
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+        return "유저 아이디: " + id + "는 삭제 되었습니다";
+    }
+}
